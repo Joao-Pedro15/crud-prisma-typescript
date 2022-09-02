03f6eccfc646a229ca3e65c68eccd7350f4ba475
João Pedro <joao.monteiro@veredaeducacao.com>
2022-09-02 18:07:23 -0300

    refactor: delete function addUser out in class

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -18,27 +18,3 @@ class UserController {
 }
 
 export default new UserController()
-
-
-export async function addUser (req: Request, res: Response) {
-    const { username, password } = req.body
-    if(!username || !password) return res.status(400).json({message: "Preencha todos os campos!"})
-    const isUsername = await prisma.user.findUnique({
-        where: {
-            username
-        }
-    })
-    if(isUsername) return res.status(409).json({message: "Username já em uso!"})
-    try{
-        const user = await prisma.user.create({
-            data:{
-                username,
-                password
-            }
-        })
-        return res.status(201).json(user)
-    }catch(err){
-        return res.status(500).json({message: "Houve um erro interno!", error: err})
-    }
-}
-
