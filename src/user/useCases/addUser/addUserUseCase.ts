a0844c8dd94d2eab73a0239177e4ef1628b228c2
João Pedro <jjoao.monteiro15@gmail.com>
2023-02-10 20:30:59 -0300

    refactor: adding notification pattern error handling

--- src/user/useCases/addUser/addUserUseCase.ts
+++ src/user/useCases/addUser/addUserUseCase.ts
@@ -1,11 +1,24 @@
 import { IAddUserRepository } from "../../repositories";
+import { UserData, UserEntity } from '../../entites/UserEntity'
+
 
 export class AddUserUseCase {
   constructor(
     private repository: IAddUserRepository
   ){}
 
-  async execute(data:any) {
-      return this.repository.add(data)
+  async execute(data:UserData) {
+    const user = new UserEntity(data)
+    if(!user.isValid()) {
+      return {
+        errors: user.notifications,
+        statusCode: 400
+      }
+    }
+    await this.repository.add(data)
+    return {
+      errors: null,
+      statusCode: 201
+    }
   }
 }
