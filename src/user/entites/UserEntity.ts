3f989e29f2bb656b7576d6c2c7955a8f68d766d6
João Pedro <jjoao.monteiro15@gmail.com>
2023-02-09 22:39:20 -0300

    feat: extends class notication patterns and adding method isValid

--- src/user/entites/UserEntity.ts
+++ src/user/entites/UserEntity.ts
@@ -1,13 +1,24 @@
+import Notification from "../../notifications/notifications"
+
 export interface UserData {
   id?: number
   username: string
   password: string
 }
 
-export class UserEntity implements UserData {
+export class UserEntity extends Notification implements UserData {
   public username: string
   public password: string
   constructor(data: UserData) {
+    super()
     Object.assign(data, this)
+  }
+
+  isValid() {
+    if(this.password.trim().length === 0) {
+      this.addNotification('username and password not empty')
+    }
+
+    return !this.hasNotifications()
   }
 }
